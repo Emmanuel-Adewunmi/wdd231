51f699ff427bb31e6e8abe9ffd88729329e5d54e
emmanuel-adewunmi <eodedina@byupathway.edu>
2025-07-22 22:12:57 +0100

    Week weather activity fulfilled

---
+++ week03/scripts/weather.js
@@ -0,0 +1,38 @@
+// select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+
+// Trier, Germany coordinates
+const apiKey = "1afd9cd4a13277b7f1684b22372a95e5"
+const lat = "49.75"
+const lon = "6.64"
+
+// Full path using template literals
+const url = `//api.openweathermap.org/data/2.5/weather?q=Trier,germanylat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data); // testing only
+            displayResults(data); // uncomment when ready
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp.toFixed(0)}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = `${desc}`;
+}
+apiFetch();
