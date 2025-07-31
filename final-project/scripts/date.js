e92878914eb2415e3debbde5a10cae570c5ebb54
emmanuel-adewunmi <eodedina@byupathway.edu>
2025-07-31 22:22:20 +0100

    Week 05 Project Siteplan fulfilled

---
+++ final-project/scripts/date.js
@@ -0,0 +1,3 @@
+// Auto-update year and last modified
+document.getElementById('currentyear').textContent = new Date().getFullYear();
+document.getElementById('lastModified').textContent = "Last Modified: " + document.lastModified;
